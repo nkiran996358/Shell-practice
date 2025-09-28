831e131edec41e88a41c70602a0784b4b25ea9a1
Azure devops <kirannaroju2022@gmail.com>
2025-09-28 16:26:42 +0530

    variable commiting

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # No space before or after equal
-PERSON1="kiran"
-PERSON2="arun"
+PERSON1="Dravid"
+PERSON2="Sachin"
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
